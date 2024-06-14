81b519f1ca898389d82756a7022b38200c064cbd
河田鷹介 <benefium@gmail.com>
2024-06-15 02:04:25 +0900

    add: core

--- includes/ft_core.hpp
+++ includes/ft_core.hpp
@@ -1,13 +1,19 @@
-#ifndef FT_CORE_H
-#define FT_CORE_H
+#ifndef FT_CORE_HPP
+#define FT_CORE_HPP
+
+#include "ft_logger.hpp"
 
 namespace shmup
 {
 	class Core
 	{
 	private:
+		Logger logger;
+
 	public:
 		Core();
+
+		Logger &log();
 	};
 
 } // namespace shmpup
---
+++ srcs/ft_core.cpp
@@ -0,0 +1,13 @@
+#include "ft_core.hpp"
+
+namespace shmup
+{
+	Core::Core()
+	{
+	}
+
+	Logger &Core::log()
+	{
+		return logger;
+	}
+} // namespace shmpup
